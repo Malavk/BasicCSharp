82040ed768851b41e05463426b8f7bff8e1c8f4e
OlegErmoshchikov <ermoshchikov@gmail.com>
2021-06-13 17:47:01 +0300

    Добавил третье дз

---
+++ DZ_tree/MainProgram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DZ_Tree
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            #region'Написать программу, выводящую элементы двухмерного массива по диагонали.
+            Diagonal one = new Diagonal();
+            one.SolutionOne();
+            #endregion
+
+            #region'Телефонный справочник
+            Phonebook two = new Phonebook();
+            two.PhonebookSolution();
+            #endregion
+
+            #region'Написать программу, выводящую введенную пользователем строку в обратном порядке (olleH вместо Hello).
+            olleH tree = new olleH();
+            tree.olleHSolution();
+            #endregion
+
+            #region'Морской бой
+            SeaBattle four = new SeaBattle();
+            four.SeaBattleSolution();
+            #endregion
+        }
+    }
+}
---
+++ DZ_tree/Phonebook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DZ_Tree
+{
+    class Phonebook
+    {
+        public void PhonebookSolution()
+        {
+            string[,] pb = new string[5, 2]
+                { {"Олег", "8910939644 / oleg@bk.ru" }, 
+                {"Владимир", "8910939655 / vladimir@bk.ru" }, 
+                { "Николай", "8910939666 / nikolay@bk.ru" }, 
+                { "Жанна", "8910939777 / zhanna@bk.ru" }, 
+                { "Петр", "891098888 / petr@bk.ru" } };
+
+            for (int i = 0; i < pb.GetLength(0); i++)
+            {
+               for (int j = 0; j < pb.GetLength(1); j++)
+                    {
+                        Console.WriteLine(pb[i,j]);
+                        Console.ReadKey();
+                    }
+            }
+        }
+    }
+}
