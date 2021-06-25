91282cb5d609d740bb8fed8f4d45286dbb75c00c
OlegErmoshchikov <ermoshchikov@gmail.com>
2021-06-25 17:44:38 +0300

    Добавил 6 дз

---
+++ DZ_six/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DZ_six
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя процесса: ");
+            string name = Console.ReadLine();
+
+            foreach(Process process in Process.GetProcesses())
+            {
+                if (process.ProcessName.StartsWith(name))
+                {
+                    process.Kill();
+                    process.WaitForExit();
+                }
+                Console.WriteLine($"ID: {process.Id}  Name: {process.ProcessName}");
+            }
+        }
+    }
+}
